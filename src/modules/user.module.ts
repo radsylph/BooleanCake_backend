5bc3ba007838e1e7a5ccb9d43a13bd0358d548dc
radsylph <andresa@devtop.io>
2024-02-17 00:03:18 -0400

    feat: added verify method

--- src/modules/user.module.ts
+++ src/modules/user.module.ts
@@ -18,13 +18,40 @@ class UserModule {
 			newUser.token = token;
 			await newUser.save();
 			return reply
-				.code(202)
-				.send({ message: "User created", data: { newUser, token } });
+				.code(200)
+				.send({ message: "User created", data: newUser });
 		} catch (error) {
 			console.log(error);
 			return reply.code(500).send({ message: "Error creating user", error });
 		}
 	}
+
+	async verifyUser(request: FastifyRequest, reply: FastifyReply) {
+		const token = request.params;
+		try {
+			const user = await User.findOne({token: token});
+			if(!user){
+				return reply.code(404).send({message: "User not found"});
+			}
+			user.token = null;
+			user.verify = true;
+			await user.save();
+			return reply.code(200).send({message: "User verified"});
+		} catch (error) {
+			return reply.code(500).send({ message: "Error verifying user", error });
+		}
+	}
+
+	test(request: FastifyRequest, reply: FastifyReply) {
+		try {
+			const params = request.params;
+
+			return reply.code(200).send({ message: "UserModule works" });	
+		} catch (error) {
+			return reply.code(500).send({ message: "Error testing UserModule", error });
+		}
+	}
+
 }
 
 export default UserModule;
