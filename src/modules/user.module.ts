0a39bf70fef079e2a5e8fc470b1449fef4d30016
radsylph <andresa@devtop.io>
2024-02-17 00:43:58 -0400

    feat: change importation

--- src/modules/user.module.ts
+++ src/modules/user.module.ts
@@ -1,7 +1,7 @@
 import bcrypt from "bcrypt";
 import { FastifyReply, FastifyRequest } from "fastify";
 import jwt from "jsonwebtoken";
-import User from "../user/models/user";
+import User from "../models/user";
 import { generateJWT, generateToken1 } from "../utils/generateToken";
 import { emailRegistro, emailReset } from "../utils/mail";
 
@@ -17,9 +17,7 @@ class UserModule {
 			const token = generateJWT(newUser._id as any);
 			newUser.token = token;
 			await newUser.save();
-			return reply
-				.code(200)
-				.send({ message: "User created", data: newUser });
+			return reply.code(200).send({ message: "User created", data: newUser });
 		} catch (error) {
 			console.log(error);
 			return reply.code(500).send({ message: "Error creating user", error });
@@ -29,14 +27,14 @@ class UserModule {
 	async verifyUser(request: FastifyRequest, reply: FastifyReply) {
 		const token = request.params;
 		try {
-			const user = await User.findOne({token: token});
-			if(!user){
-				return reply.code(404).send({message: "User not found"});
+			const user = await User.findOne({ token: token });
+			if (!user) {
+				return reply.code(404).send({ message: "User not found" });
 			}
 			user.token = null;
 			user.verify = true;
 			await user.save();
-			return reply.code(200).send({message: "User verified"});
+			return reply.code(200).send({ message: "User verified" });
 		} catch (error) {
 			return reply.code(500).send({ message: "Error verifying user", error });
 		}
@@ -45,12 +43,13 @@ class UserModule {
 	test(request: FastifyRequest, reply: FastifyReply) {
 		try {
 			const params = request.params;
-			return reply.code(200).send({ message: "UserModule works" });	
+			return reply.code(200).send({ message: "UserModule works" });
 		} catch (error) {
-			return reply.code(500).send({ message: "Error testing UserModule", error });
+			return reply
+				.code(500)
+				.send({ message: "Error testing UserModule", error });
 		}
 	}
-
 }
 
 export default UserModule;
