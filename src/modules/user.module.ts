d600007e46357318b92a8f4dcdd7ceff871fc1e2
radsylph <andresa@devtop.io>
2024-02-17 01:48:33 -0400

    feat: added the email service to the create user method and fixed the params for the verifyUser method

--- src/modules/user.module.ts
+++ src/modules/user.module.ts
@@ -1,6 +1,8 @@
 import bcrypt from "bcrypt";
 import { FastifyReply, FastifyRequest } from "fastify";
+import { RouteGenericInterface } from "fastify/types/route";
 import jwt from "jsonwebtoken";
+import { UserInterface } from "../interfaces";
 import User from "../models/user";
 import { generateJWT, generateToken1 } from "../utils/generateToken";
 import { emailRegistro, emailReset } from "../utils/mail";
@@ -11,12 +13,33 @@ class UserModule {
 	}
 
 	async createUser(request: FastifyRequest, reply: FastifyReply) {
-		const user_info = request.body;
+		const user_info: any = request.body;
 		try {
+			const existingEmail = await User.findOne({ email: user_info.email });
+			if (existingEmail) {
+				return reply.code(500).send({ message: "Email already in use" });
+			}
+			const exitingUsername = await User.findOne({
+				username: user_info.username,
+			});
+			if (exitingUsername) {
+				return reply.code(500).send({ message: "Username already in use" });
+			}
 			const newUser = await User.create(user_info);
-			const token = generateJWT(newUser._id as any);
+			const token = generateToken1();
+			try {
+				emailRegistro({
+					email: newUser.email,
+					nombre: newUser.name,
+					token: token,
+				});
+			} catch (error) {
+				console.log(error);
+				return reply.code(500).send({ message: "Error sending email", error });
+			}
 			newUser.token = token;
 			await newUser.save();
+
 			return reply.code(200).send({ message: "User created", data: newUser });
 		} catch (error) {
 			console.log(error);
@@ -25,8 +48,10 @@ class UserModule {
 	}
 
 	async verifyUser(request: FastifyRequest, reply: FastifyReply) {
-		const token = request.params;
+		const { token } = request.params as { token: string };
 		try {
+			console.log(token);
+			console.log(typeof token);
 			const user = await User.findOne({ token: token });
 			if (!user) {
 				return reply.code(404).send({ message: "User not found" });
@@ -34,7 +59,7 @@ class UserModule {
 			user.token = null;
 			user.verify = true;
 			await user.save();
-			return reply.code(200).send({ message: "User verified" });
+			return reply.code(200).send({ message: "User verified", user });
 		} catch (error) {
 			return reply.code(500).send({ message: "Error verifying user", error });
 		}
