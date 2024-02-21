241ebd7d8c281bcaacd1e5db8e5d339a400a5339
radsylph <andresa@devtop.io>
2024-02-21 17:09:47 -0400

    feat:added controllers for the user login, logout, and to get the user information given the session cookie plus some tests

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -34,6 +34,35 @@ const test4 = async (request: FastifyRequest, reply: FastifyReply) => {
 	userModule.test(request, reply);
 };
 
+const test5 = async (request: FastifyRequest, reply: FastifyReply) => {
+	try {
+		const cookie: string = "volaro";
+		reply.setCookie("session", cookie);
+		return reply.code(200).send({ message: "cookie" });
+	} catch (error) {
+		return reply.code(500).send({ message: "error" });
+	}
+};
+
+const test6 = async (request: FastifyRequest, reply: FastifyReply) => {
+	try {
+		const cookie = request.cookies.session;
+		return reply.code(200).send({ message: cookie });
+	} catch (error) {
+		return reply.code(500).send({ message: "error" });
+	}
+};
+
+const test7 = async (request: FastifyRequest, reply: FastifyReply) => {
+	try {
+		//reply.setCookie("session", "", { expires: new Date(0) });
+		reply.clearCookie("session");
+		return reply.code(200).send({ message: "Cookie destruida" });
+	} catch (error) {
+		return reply.code(500).send({ message: "error" });
+	}
+};
+
 const createUser = async (request: FastifyRequest, reply: FastifyReply) => {
 	await userModule.createUser(request, reply);
 };
@@ -42,4 +71,29 @@ const verifyUser = async (request: FastifyRequest, reply: FastifyReply) => {
 	await userModule.verifyUser(request, reply);
 };
 
-export { test1, test2, test3, createUser, test4, verifyUser };
+const loginUser = async (request: FastifyRequest, reply: FastifyReply) => {
+	await userModule.loginUser(request, reply);
+};
+
+const getUserInfo = async (request: FastifyRequest, reply: FastifyReply) => {
+	await userModule.getUserInfo(request, reply);
+};
+
+const logOutUser = async (request: FastifyRequest, reply: FastifyReply) => {
+	await userModule.logOutUser(request, reply);
+};
+
+export {
+	test1,
+	test2,
+	test3,
+	test4,
+	test5,
+	test6,
+	test7,
+	createUser,
+	verifyUser,
+	loginUser,
+	getUserInfo,
+	logOutUser,
+};
