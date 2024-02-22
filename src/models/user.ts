7cfc49d0a7169c59ace6587997230a6d096fe871
radsylph <andresa@devtop.io>
2024-02-21 22:30:56 -0400

    feat:deleted some unnecesary code

--- src/models/user.ts
+++ src/models/user.ts
@@ -12,11 +12,6 @@ const usuarioSchema = new mongoose.Schema<UserInterface>(
 			type: String,
 			required: true,
 		},
-		// username: {
-		// 	type: String,
-		// 	required: true,
-		// 	unique: true,
-		// },
 		email: {
 			type: String,
 			required: true,
