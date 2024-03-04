e1bdcf1e65d30c4bcaa5cec12ad12720ede9e5fd
radsylph <andresa@devtop.io>
2024-03-04 12:39:04 -0400

    feat: deleted tests and unnecesarry comments, and added dependencies injection

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -1,67 +1,7 @@
 import fastify from "fastify";
-import { FastifyReply, FastifyRequest } from "fastify";
-import UserModule from "../modules/user.module";
-
-const userModule = new UserModule();
-
-const test1 = async (request: FastifyRequest, reply: FastifyReply) => {
-	const params = request.body;
-	try {
-		return reply.code(200).send({ message: "hello", data: params });
-	} catch (error) {
-		return reply.code(500).send({ message: "error" });
-	}
-};
-
-const test2 = async (request: FastifyRequest, reply: FastifyReply) => {
-	try {
-		return reply.code(200).send({ message: "hello" });
-	} catch (error) {
-		return reply.code(500).send({ message: "error" });
-	}
-};
-
-const test3 = async (request: FastifyRequest, reply: FastifyReply) => {
-	try {
-		const params = request.params;
-		return reply.code(200).send({ params: params });
-	} catch (error) {
-		return reply.code(500).send({ message: "error" });
-	}
-};
-
-const test4 = async (request: FastifyRequest, reply: FastifyReply) => {
-	userModule.test(request, reply);
-};
-
-const test5 = async (request: FastifyRequest, reply: FastifyReply) => {
-	try {
-		const cookie: string = "volaro";
-		reply.setCookie("session", cookie);
-		return reply.code(200).send({ message: "cookie" });
-	} catch (error) {
-		return reply.code(500).send({ message: "error" });
-	}
-};
-
-const test6 = async (request: FastifyRequest, reply: FastifyReply) => {
-	try {
-		const cookie = request.cookies.session;
-		return reply.code(200).send({ message: cookie });
-	} catch (error) {
-		return reply.code(500).send({ message: "error" });
-	}
-};
-
-const test7 = async (request: FastifyRequest, reply: FastifyReply) => {
-	try {
-		//reply.setCookie("session", "", { expires: new Date(0) });
-		reply.clearCookie("session");
-		return reply.code(200).send({ message: "Cookie destruida" });
-	} catch (error) {
-		return reply.code(500).send({ message: "error" });
-	}
-};
+import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import { UCM } from "../config/container";
+const userModule = UCM.resolve("UserModule");
 
 const createUser = async (request: FastifyRequest, reply: FastifyReply) => {
 	await userModule.createUser(request, reply);
@@ -83,17 +23,20 @@ const logOutUser = async (request: FastifyRequest, reply: FastifyReply) => {
 	await userModule.logOutUser(request, reply);
 };
 
+const getSchemas = async (
+	request: FastifyRequest,
+	reply: FastifyReply,
+	fastify: FastifyInstance,
+) => {
+	const schemas = fastify.getSchemas();
+	await reply.code(200).send(schemas);
+};
+
 export {
-	test1,
-	test2,
-	test3,
-	test4,
-	test5,
-	test6,
-	test7,
 	createUser,
 	verifyUser,
 	loginUser,
 	getUserInfo,
 	logOutUser,
+	getSchemas,
 };
