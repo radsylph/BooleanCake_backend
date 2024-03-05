3de30fedfaafabc3cb68fa1169f375a4b3cf834d
Fernando Parra <thefer767@gmail.com>
2024-03-04 20:11:13 -0400

    idk

--- dist/routes/product.routes.js
+++ dist/routes/product.routes.js
@@ -6,31 +6,31 @@ const product_controller_1 = require("../controllers/product.controller");
 // 	fastify.post("/create", createproducts);
 // };
 function productsRouter(fastify, opts, done) {
-    fastify.route({
-        method: "GET",
-        url: "/getall",
-        handler: product_controller_1.getAllProducts,
-    });
-    fastify.route({
-        method: "PUT",
-        url: "/update",
-        handler: product_controller_1.updateProduct,
-    });
-    fastify.route({
-        method: "POST",
-        url: "/create",
-        handler: product_controller_1.createProduct,
-    });
-    fastify.route({
-        method: "POST",
-        url: "/get",
-        handler: product_controller_1.getProduct,
-    });
-    fastify.route({
-        method: "DELETE",
-        url: "/delete",
-        handler: product_controller_1.deleteProduct,
-    });
-    done();
+	fastify.route({
+		method: "GET",
+		url: "/getall",
+		handler: product_controller_1.getAllProducts,
+	});
+	fastify.route({
+		method: "PUT",
+		url: "/update",
+		handler: product_controller_1.updateProduct,
+	});
+	fastify.route({
+		method: "POST",
+		url: "/create",
+		handler: product_controller_1.createProduct,
+	});
+	fastify.route({
+		method: "POST",
+		url: "/get",
+		handler: product_controller_1.getProduct,
+	});
+	fastify.route({
+		method: "DELETE",
+		url: "/delete",
+		handler: product_controller_1.deleteProduct,
+	});
+	done();
 }
 exports.default = productsRouter;
