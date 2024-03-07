2b9df0950942868b11a2cf45b9d2b2b3c76bdaab
radsylph <andresa@devtop.io>
2024-03-07 00:34:32 -0400

    dist

--- dist/config/container.js
+++ dist/config/container.js
@@ -3,16 +3,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.PCM = exports.UCM = void 0;
+exports.ICM = exports.PCM = exports.UCM = void 0;
 const awilix_1 = require("awilix");
+const ingredient_1 = __importDefault(require("../models/ingredient"));
 const product_1 = __importDefault(require("../models/product"));
 const user_1 = __importDefault(require("../models/user"));
+const ingredient_module_1 = __importDefault(require("../modules/ingredient.module"));
 const product_module_1 = __importDefault(require("../modules/product.module"));
 const user_module_1 = __importDefault(require("../modules/user.module"));
 const UCM = (0, awilix_1.createContainer)(); //User Container Module
 exports.UCM = UCM;
-const PCM = (0, awilix_1.createContainer)();
+const PCM = (0, awilix_1.createContainer)(); //Product Container Module
 exports.PCM = PCM;
+const ICM = (0, awilix_1.createContainer)(); //Invoice Container Module
+exports.ICM = ICM;
 UCM.register({
     UserModule: (0, awilix_1.asClass)(user_module_1.default).singleton(),
     userModel: (0, awilix_1.asValue)(user_1.default),
@@ -20,4 +24,8 @@ UCM.register({
 PCM.register({
     ProductModule: (0, awilix_1.asClass)(product_module_1.default).singleton(),
     ProductModel: (0, awilix_1.asValue)(product_1.default),
+});
+ICM.register({
+    IngridientModule: (0, awilix_1.asClass)(ingredient_module_1.default).singleton(),
+    IngredientModel: (0, awilix_1.asValue)(ingredient_1.default),
 });
--- dist/modules/ingredient.module.js
+++ dist/modules/ingredient.module.js
@@ -1,1 +1,120 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+class IngredientModule {
+    constructor({ IngredientModel, }) {
+        this.Ingridient = IngredientModel;
+        console.log("IngredientModule loaded");
+    }
+    createIngredient(request, reply) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const { name, price, stock, unitMeasure } = request.body;
+            try {
+                const newIngredient = yield this.Ingridient.create({
+                    name,
+                    price,
+                    stock,
+                    unitMeasure,
+                });
+                yield newIngredient.save();
+                return reply
+                    .code(202)
+                    .send({ message: "Ingredient created", data: newIngredient });
+            }
+            catch (error) {
+                console.log(error);
+                return reply
+                    .code(500)
+                    .send({ message: "Error creating Ingredient", error });
+            }
+        });
+    }
+    updateIngredient(request, reply) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const { id } = request.params;
+            const { name, price, stock, unitMeasure } = request.body;
+            try {
+                const ExistingIngrient = yield this.Ingridient.findById(id);
+                if (!ExistingIngrient) {
+                    return reply.code(404).send({ message: "Ingredient not found" });
+                }
+                const updatedIngredient = yield this.Ingridient.findByIdAndUpdate({
+                    name,
+                    price,
+                    stock,
+                    unitMeasure,
+                });
+                return reply
+                    .code(202)
+                    .send({ message: "Ingredient updated", data: updatedIngredient });
+            }
+            catch (error) {
+                return reply
+                    .code(500)
+                    .send({ message: "Error updating Ingredient", error });
+            }
+        });
+    }
+    deleteIngredient(request, reply) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const { id } = request.params;
+            try {
+                const deletedIngredient = yield this.Ingridient.findByIdAndDelete(id);
+                if (!deletedIngredient) {
+                    return reply.code(404).send({ message: "Ingredient not found" });
+                }
+                return reply
+                    .code(202)
+                    .send({ message: "Ingredient deleted", data: deletedIngredient });
+            }
+            catch (error) {
+                return reply
+                    .code(500)
+                    .send({ message: "Error deleting Ingredient", error });
+            }
+        });
+    }
+    getAllIngridients(request, reply) {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                const ingredients = yield this.Ingridient.find();
+                return reply
+                    .code(200)
+                    .send({ message: "ingridients founds", data: ingredients });
+            }
+            catch (error) {
+                return reply
+                    .code(500)
+                    .send({ message: "Error getting ingredients", error });
+            }
+        });
+    }
+    getIngridientById(request, reply) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const { id } = request.params;
+            try {
+                const ingredient = yield this.Ingridient.findById(id);
+                if (!ingredient) {
+                    return reply.code(404).send({ message: "Ingredient not found" });
+                }
+                return reply
+                    .code(200)
+                    .send({ message: "Ingredient found", data: ingredient });
+            }
+            catch (error) {
+                return reply
+                    .code(500)
+                    .send({ message: "Error getting ingredient", error });
+            }
+        });
+    }
+}
+exports.default = IngredientModule;
