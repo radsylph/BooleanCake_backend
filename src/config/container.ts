2d942b63dba116948e81c25b861828fbed32189a
Fernando Parra <thefer767@gmail.com>
2024-03-07 00:48:27 -0400

    Regiones

--- src/config/container.ts
+++ src/config/container.ts
@@ -1,9 +1,11 @@
 import { asClass, asValue, createContainer } from "awilix";
 import Ingredient from "../models/ingredient";
 import Product from "../models/product";
+import Region from "../models/region";
 import User from "../models/user";
 import IngredientModule from "../modules/ingredient.module";
 import ProductsModule from "../modules/product.module";
+import RegionModule from "../modules/region.module";
 import UserModule from "../modules/user.module";
 
 const UCM = createContainer(); //User Container Module
@@ -11,6 +13,8 @@ const UCM = createContainer(); //User Container Module
 const PCM = createContainer(); //Product Container Module
 
 const ICM = createContainer(); //Invoice Container Module
+
+const RCM = createContainer();
 
 UCM.register({
 	UserModule: asClass(UserModule).singleton(),
@@ -27,4 +31,9 @@ ICM.register({
 	IngredientModel: asValue(Ingredient),
 });
 
-export { UCM, PCM, ICM };
+RCM.register({
+	RegionModule: asClass(RegionModule).singleton(),
+	RegionModel: asValue(Region),
+});
+
+export { UCM, PCM, ICM, RCM };
