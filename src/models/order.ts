260ad4bd52680fdf0956c614f08f6cb962ea852b
radsylph <andresa@devtop.io>
2024-04-10 18:27:17 -0400

    feat:update order model and added extra fields and timestaps, have to work in the incremental serial

--- src/models/order.ts
+++ src/models/order.ts
@@ -1,31 +1,55 @@
 import mongoose from "mongoose";
 import { OrderInterface } from "../interfaces/order.interface";
 
-const OrderSchema = new mongoose.Schema<OrderInterface>({
-	owner: {
-		type: String,
-		ref: "User",
-		required: true,
+const OrderSchema = new mongoose.Schema<OrderInterface>(
+	{
+		serial: {
+			type: Number,
+			required: true,
+			default: 0,
+		},
+		owner: {
+			type: String,
+			ref: "User",
+			required: true,
+		},
+		rider: {
+			type: String,
+			ref: "User",
+			required: true,
+			default: "none",
+		},
+		location: {
+			type: String,
+			ref: "Location",
+			required: true,
+			default: "none",
+		},
+		status: {
+			type: String,
+			enum: ["pending", "in progress", "ready", "delivered"],
+			default: "pending",
+			required: true,
+		},
+		totalPrice: {
+			type: Number,
+			required: true,
+		},
+		isExpress: {
+			type: Boolean,
+			required: true,
+			default: false,
+		},
+		isDelivery: {
+			type: Boolean,
+			required: true,
+			default: false,
+		},
 	},
-	rider: {
-		type: String,
-		ref: "User",
-		required: true,
-		default: "none",
+	{
+		timestamps: true,
 	},
-	location: {
-		type: String,
-		ref: "Location",
-		required: true,
-		default: "none",
-	},
-	status: {
-		type: String,
-		enum: ["pending", "in progress", "ready", "delivered"],
-		default: "pending",
-		required: true,
-	},
-});
+);
 
 const Order = mongoose.model("Order", OrderSchema);
 
